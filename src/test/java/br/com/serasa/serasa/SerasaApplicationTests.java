bf075a7b7a6844cf8f08a79e98b616d7a1f56542
Thiago Avancini <thiagoavancini@outlook.com>
2026-08-01 21:37:05 -0300

    first commit

---
+++ src/test/java/br/com/serasa/serasa/SerasaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.serasa.serasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerasaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
